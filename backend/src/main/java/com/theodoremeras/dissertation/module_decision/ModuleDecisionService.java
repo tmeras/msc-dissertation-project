465756b0a9a09be077b254ff0a6ba04ad5c3c3d6
tmeras <tmeras@yahoo.gr>
2024-07-23 16:15:44 +0100

    Implement module decision service

---
+++ backend/src/main/java/com/theodoremeras/dissertation/module_decision/ModuleDecisionService.java
@@ -0,0 +1,41 @@
+package com.theodoremeras.dissertation.module_decision;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ModuleDecisionService {
+
+    private ModuleDecisionRepository moduleDecisionRepository;
+
+    public ModuleDecisionService(ModuleDecisionRepository moduleDecisionRepository) {
+        this.moduleDecisionRepository = moduleDecisionRepository;
+    }
+
+    public ModuleDecisionEntity save(ModuleDecisionEntity moduleDecisionEntity) {
+        return moduleDecisionRepository.save(moduleDecisionEntity);
+    }
+
+    public List<ModuleDecisionEntity> findAll() {
+        return moduleDecisionRepository.findAll();
+    }
+
+    public List<ModuleDecisionEntity> findAllByModuleRequestId(Integer moduleRequestId) {
+        return moduleDecisionRepository.findAllByModuleRequestId(moduleRequestId);
+    }
+
+    public Optional<ModuleDecisionEntity> findOneById(Integer id) {
+        return moduleDecisionRepository.findById(id);
+    }
+
+    public Boolean exists(Integer id) {
+        return moduleDecisionRepository.existsById(id);
+    }
+
+    public void delete(Integer id) {
+        moduleDecisionRepository.deleteById(id);
+    }
+
+}
