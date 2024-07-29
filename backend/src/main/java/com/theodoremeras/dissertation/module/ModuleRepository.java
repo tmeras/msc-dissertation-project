dc26fbfb6326eba3a0044d1fce191f19768a1e07
tmeras <tmeras@yahoo.gr>
2024-07-29 18:07:46 +0100

    Implement functionality for getting modules by a list of module codes

--- backend/src/main/java/com/theodoremeras/dissertation/module/ModuleRepository.java
+++ backend/src/main/java/com/theodoremeras/dissertation/module/ModuleRepository.java
@@ -1,10 +1,14 @@
 package com.theodoremeras.dissertation.module;
 
+import com.theodoremeras.dissertation.module_request.ModuleRequestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleEntity, String> {
 
+    List<ModuleEntity> findAllByCodeIn(List<String> codes);
 
 }
