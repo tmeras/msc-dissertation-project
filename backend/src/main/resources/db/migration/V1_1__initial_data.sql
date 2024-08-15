1701d2758559c3f07d687c09694c0960519affaf
tmeras <tmeras@yahoo.gr>
2024-08-15 09:42:00 +0100

    Backend - Update database migration script

--- backend/src/main/resources/db/migration/V1_1__initial_data.sql
+++ backend/src/main/resources/db/migration/V1_1__initial_data.sql
@@ -24,11 +24,11 @@ SELECT NEXTVAL('_user_seq');
 SELECT NEXTVAL('_user_seq');
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(1, 'Student (Dept.1-1)', 'tmeras@yahoo.gr', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 1, 1);
+VALUES(1, 'Student A (Dept.1)', 'tmeras@yahoo.gr', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 1, 1);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(2, 'Student (Dept.1-2)', 'tmeras1@sheffield.ac.uk', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 1, 1);
+VALUES(2, 'Student B (Dept.1)', 'tmeras1@sheffield.ac.uk', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 1, 1);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
@@ -44,27 +44,27 @@ VALUES(5, 'Clerical Staff (Dept.2)', 'clstaff2@gmail.com', '$2a$10$jUQ5ifdVbHENC
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(6, 'Academic Staff (Dept.1-1)', 'acstaff1@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 1);
+VALUES(6, 'Academic Staff A (Dept.1)', 'acstaff1@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 1);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(7, 'Academic Staff (Dept.1-2)', 'acstaff2@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 1);
+VALUES(7, 'Academic Staff B (Dept.1)', 'acstaff2@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 1);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(8, 'Academic Staff (Dept.1-3)', 'acstaff3@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 1);
+VALUES(8, 'Academic Staff C (Dept.1)', 'acstaff3@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 1);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(9, 'Academic Staff (Dept.2-1)', 'acstaff4@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 2);
+VALUES(9, 'Academic Staff A (Dept.2)', 'acstaff4@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 2);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(10, 'Academic Staff (Dept.2-2)', 'acstaff5@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 2);
+VALUES(10, 'Academic Staff B (Dept.2)', 'acstaff5@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 2);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
-VALUES(11, 'Academic Staff (Dept.2-3)', 'acstaff6@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 2);
+VALUES(11, 'Academic Staff C (Dept.2)', 'acstaff6@gmail.com', '$2a$10$jUQ5ifdVbHENCVx68ZyMSelRnFSJH60YBMP3ejhKi5MLCnw54GpTS', true, 3, 2);
 
 INSERT INTO
 _user(id, name, email, password, is_approved, role_id, department_id)
@@ -94,11 +94,19 @@ VALUES(1, 'I fell victim to a violent crime', TO_DATE('2024/05/03','YYYY/MM/DD')
 
 INSERT INTO
 ec_application(id, circumstances_details, affected_date_start, affected_date_end, submitted_on, requires_further_evidence, is_referred, student_id)
-VALUES(2, 'I was in a car accident', TO_DATE('2024/06/12','YYYY/MM/DD'),TO_DATE('2024/6/17','YYYY/MM/DD'), TO_DATE('2024/6/25','YYYY/MM/DD'), false, false, 2);
+VALUES(2, 'I was in a car accident', TO_DATE('2024/05/12','YYYY/MM/DD'),TO_DATE('2024/05/17','YYYY/MM/DD'), TO_DATE('2024/05/25','YYYY/MM/DD'), false, false, 2);
 
 INSERT INTO
 ec_application(id, circumstances_details, affected_date_start, affected_date_end, submitted_on, requires_further_evidence, is_referred, student_id)
-VALUES(3, 'My grandmother passed away', TO_DATE('2024/06/15','YYYY/MM/DD'),TO_DATE('2024/6/17','YYYY/MM/DD'), TO_DATE('2024/6/25','YYYY/MM/DD'), false, true, 3);
+VALUES(3, 'My grandmother passed away', TO_DATE('2024/06/15','YYYY/MM/DD'),TO_DATE('2024/06/17','YYYY/MM/DD'), TO_DATE('2024/06/25','YYYY/MM/DD'), false, true, 3);
+
+INSERT INTO
+ec_application(id, circumstances_details, affected_date_start, affected_date_end, submitted_on, requires_further_evidence, is_referred, student_id)
+VALUES(4, 'I had jury service during this period', TO_DATE('2024/05/22','YYYY/MM/DD'),TO_DATE('2024/05/29','YYYY/MM/DD'), TO_DATE('2024/05/29','YYYY/MM/DD'), true, null, 1);
+
+INSERT INTO
+ec_application(id, circumstances_details, affected_date_start, affected_date_end, submitted_on, requires_further_evidence, is_referred, student_id)
+VALUES(5, 'I was in the hospital', TO_DATE('2024/06/24','YYYY/MM/DD'),TO_DATE('2024/06/27','YYYY/MM/DD'), TO_DATE('2024/06/27','YYYY/MM/DD'), false, true, 1);
 
 
 SELECT NEXTVAL('module_outcome_request_seq');
@@ -126,6 +134,18 @@ VALUES(5, 'Remove Lateness Penalties', 2, 'MEC117');
 INSERT INTO
 module_outcome_request(ID, REQUESTED_OUTCOME, APPLICATION_ID, MODULE_CODE)
 VALUES(6, 'Disregard Missing Component Mark', 3, 'COM1002');
+
+INSERT INTO
+module_outcome_request(ID, REQUESTED_OUTCOME, APPLICATION_ID, MODULE_CODE)
+VALUES(7, 'Disregard Missing Component Mark', 4, 'COM1003');
+
+INSERT INTO
+module_outcome_request(ID, REQUESTED_OUTCOME, APPLICATION_ID, MODULE_CODE)
+VALUES(8, 'Remove Lateness Penalties', 5, 'MEC117');
+
+INSERT INTO
+module_outcome_request(ID, REQUESTED_OUTCOME, APPLICATION_ID, MODULE_CODE)
+VALUES(9, 'Defer Formal Examination', 5, 'MEC113');
 
 
 SELECT NEXTVAL('module_request_decision_seq');
@@ -165,3 +185,11 @@ VALUES(8, null, true, 6, 9, 3);
 INSERT INTO
 module_request_decision(ID, COMMENTS, IS_APPROVED, MODULE_REQUEST_ID, STAFF_ID, APPLICATION_ID)
 VALUES(9, null, true, 6, 10, 3);
+
+INSERT INTO
+module_request_decision(ID, COMMENTS, IS_APPROVED, MODULE_REQUEST_ID, STAFF_ID, APPLICATION_ID)
+VALUES(10, null, true, 8, 6, 5);
+
+INSERT INTO
+module_request_decision(ID, COMMENTS, IS_APPROVED, MODULE_REQUEST_ID, STAFF_ID, APPLICATION_ID)
+VALUES(11, null, true, 8, 7, 5);
